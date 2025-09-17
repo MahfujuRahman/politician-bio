ab9d5e0c6559b1f25b4d6f56c42973b96a987663
MahfujuRahman <akashajmain0@gmail.com>
2025-09-17 10:11:23 +0600

    added tag manager in master

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -2,6 +2,19 @@
 <html class="no-js" lang="zxx">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0PM08PK3E8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-0PM08PK3E8');
+    </script>
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     @php
